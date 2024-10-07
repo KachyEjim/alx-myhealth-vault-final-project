50447880f6396b03433666e992b236ad9c3cac1a
Buchiken11 <buchiken025@gmail.com>
2024-10-07 07:38:13 +0100

    created CRUD

--- api/views/routes.py
+++ api/views/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, jasonify, requests
+from flask import Flask, jasonify, requests, abort
 from api import db
 from models.user import User
 from models.medical_records import MedicalRecords
